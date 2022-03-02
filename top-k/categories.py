abcc4cf989f5e5a01a52b69c00a4e2dbd59d0542
quincinia <eanj13@gmail.com>
2022-03-01 18:29:05 -0800

    Top-k categories program flow

--- top-k/categories.py
+++ top-k/categories.py
@@ -1,1 +1,22 @@
-# Add more files as needed
+# Import command line parser
+# Import DataFrame formatter
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+sc = SparkContext("local")
+spark = SparkSession(sc)
+
+if __name__ == '__main__':
+    # Get args from command line parser
+    args = []
+
+    # Initialize dataframe from dataframe formatter
+    df = args[0]
+
+    # Run query
+    # Filter columns as needed
+    df.groupBy('category').count().orderBy('count', ascending=False).show()
+
+
+
+
