9d3c831f18685a9d58361d4f3cff497fd9cf8708
quincinia <eanj13@gmail.com>
2022-03-01 16:49:51 -0800

    Made schema for reference (unused)

--- dataframes/schema.py
+++ dataframes/schema.py
@@ -1,1 +1,17 @@
 # For reading data from the dataset
+
+import pyspark
+from pyspark.sql.types import *
+
+videoSchema = StructType([
+    StructField("videoID", StringType()),
+    StructField("uploader", StringType()),
+    StructField("age", IntegerType()),
+    StructField("category", StringType()),
+    StructField("length", IntegerType()),
+    StructField("views", IntegerType()),
+    StructField("rate", FloatType()),
+    StructField("ratings", IntegerType()),
+    StructField("comments", IntegerType()),
+    StructField("relatedIDs", ArrayType(StringType()))
+])
